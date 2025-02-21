8d67b4268e2373177c7185cee4a6a918fe394d6e
Mocha366 <cerm1240364@gn.iwasaki.ac.jp>
2025-02-21 10:49:56 +0900

    ServiceのPathを変えました。 MenuのCssを調整しました。 DashボードのCssを調整しました。 通知のCssを調整しました。 SkillMatching説明欄にMenu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,7 @@
 import "./Menu.css"
 
 const links = [
-    { name: 'サービス', href: '#1', },
+    { name: 'サービス', href: '/dashboard/Service', },
     { name: 'Skill Matchingについて',href: '/dashboard/skill-matching', },
     { name: 'プロフィール編集', href: '/dashboard/profile-edit', },
     { name: 'お知らせ', href: '/dashboard/Activitie', },
