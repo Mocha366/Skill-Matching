416859f4e570a21b8f15cd6a5bf7e9ab07f68098
Hina03 <hinano.326.20@gmail.com>
2025-02-27 12:39:38 +0900

    Serviceのcss

--- src/pages/Dashboard/HeaderBar/Service.tsx
+++ src/pages/Dashboard/HeaderBar/Service.tsx
@@ -2,36 +2,43 @@ import React from "react";
 import HeaderBar from "../../../components/HeaderBar/HeaderBar";
 import FooterBar from "../../../components/Footer/FooterBar";
 import Menu from "../../../components/Menu";
+import Background from "../../../components/Background";
 
 
 const Service: React.FC = () => {
     return (
-        <div className="service-page">
-            <HeaderBar />
-            <div className="service-Menu">
-                <Menu/>
-            </div>
+        <>
             <div className="service-container">
-                <h1>さあああああああああああああああああああびす</h1>
-                <p>
-                    わたしたちはエンジニア同士の恋を応援するアプリを作りました。
-                    これは、エンジニア同士のマッチングアプリです。
-                    あなたの理想のエンジニアと出会うためのアプリです。
-                    あなたの理想のエンジニアと出会うためのアプリです。
-                    あなたの理想のエンジニアと出会うためのアプリです。
-                </p>
-                <p>
-                    あなたの理想のエンジニアと出会うためのアプリです。
-                    あなたの理想のエンジニアと出会うためのアプリです。
-                    あなたの理想のエンジニアと出会うためのアプリです。
-                    あなたの理想のエンジニアと出会うためのアプリです。
-                </p>
-                <button className="start-button">始める</button>
+                <header className="service-header">
+                    <HeaderBar />
+                </header>
+                <Background />
+                <div className="service-contents">
+                    <div className="service-menu">
+                        <Menu/>
+                    </div>
+                    <div className="test">
+                        <h1>さあああああああああああああああああああびす</h1>
+                        <p>
+                            わたしたちはエンジニア同士の恋を応援するアプリを作りました。
+                            これは、エンジニア同士のマッチングアプリです。
+                            あなたの理想のエンジニアと出会うためのアプリです。
+                            あなたの理想のエンジニアと出会うためのアプリです。
+                            あなたの理想のエンジニアと出会うためのアプリです。
+                        </p>
+                        <p>
+                            あなたの理想のエンジニアと出会うためのアプリです。
+                            あなたの理想のエンジニアと出会うためのアプリです。
+                            あなたの理想のエンジニアと出会うためのアプリです。
+                            あなたの理想のエンジニアと出会うためのアプリです。
+                        </p>
+                    </div>
+                </div>
+                <footer>
+                    <FooterBar />
+                </footer>
             </div>
-            <div>
-                <FooterBar />
-            </div>
-        </div>
+        </>
     );
 };
 
